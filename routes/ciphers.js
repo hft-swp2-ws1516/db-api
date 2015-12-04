791b373d85bcab337a3c4bd7a08de40af6e47d8d
TheJetlag <TheJetlag@users.noreply.github.com>
2015-12-04 21:23:53 +0100

    ciphers: use TLD_UNSPECIFIED constant when looking without tld parameter

--- routes/ciphers.js
+++ routes/ciphers.js
@@ -1,12 +1,15 @@
 (function () {
     'use strict';
+
     var router = require('express').Router();
     var CipherSummary = require('../schemas/cipherSummarySchema');
+
+    var TLD_UNSPECIFIED = "__all";
 
     router.route('/summary')
         .get(function(req, res, next) {
 
-            var tld = req.query.tld || null;
+            var tld = req.query.tld || TLD_UNSPECIFIED;
             var query = {
                 tld: tld
             };
