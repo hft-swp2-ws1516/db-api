71842210432a045966c10258be4764ae80e5688a
Georg Kasper <georg.kasper08@gmail.com>
2015-12-18 13:34:09 -0800

    added overview and tld

--- routes/keysize.js
+++ routes/keysize.js
@@ -1,15 +1,35 @@
-(function () {
+(function() {
     'use strict';
+    
     var router = require('express').Router();
     var KeysizeSummary = require('../schemas/keysizeSummarySchema');
-
-    router.route('/keysize')
-        .get(function(req, res, next) {
-        	KeysizeSummary.find().exec(function(err, result){
-                if (err) { res.status(500).send(err); }
-                res.status(200).json(result);
-                res.end();
-            });
-        });
+    var keysizeTLD = require('../schemas/keysizeTLDSchema');
+    
+    var TLD_UNSPECIFIED = "__all";
+    
+    router.route('/tld').get(function(req, res, next) {
+	// TODO: unspecific does not work, can't find out yet why
+	var query = {
+                tld: req.query.tld || TLD_UNSPECIFIED
+            };
+	
+	keysizeTLD.find(query).exec(function(err, result) {
+	    if (err) { res.status(500).send(err); }
+	    res.status(200).json(result);
+	    res.end();
+	});
+    });
+    
+    router.route('/overview').get(function(req, res, next) {
+	KeysizeSummary.find().exec(function(err, result) {
+	    if (err) {
+		res.status(500).send(err);
+	    }
+	    res.status(200).json(result);
+	    res.end();
+	});
+    });
+    
+    
     module.exports = router;
 }());
