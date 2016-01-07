a4aad0c76d305b05d2cbf5b2d0258a7bcad17047
Georg Kasper <georg.kasper08@gmail.com>
2016-01-07 02:53:37 -0800

    refactoring, removed tabs / whitespaces

--- schemas/keysizeTLDSchema.js
+++ schemas/keysizeTLDSchema.js
@@ -1,26 +1,26 @@
 (function() {
     'use strict';
-
+    
     var mongoose = require('mongoose');
     var Schema = mongoose.Schema;
-
+    
     // keysizeOverview Schema
     var keysizeTLDSchema = new Schema({
-	month : String,
-	tld : String,
-	sumTLD: [{
-	    publicKeyLength : Number,
-	    totalAmount : Number
-	}]
+        month : String,
+        tld : String,
+        sumTLD: [{
+            publicKeyLength : Number,
+            totalAmount : Number
+        }]
     }, {
-	toObject : {
-	    virtuals : true
-	},
-	toJSON : {
-	    virtuals : true
-	}
+    toObject : {
+        virtuals : true
+    },
+    toJSON : {
+        virtuals : true
+    }
     });
-
+    
     // Create Model using the keysizeSchema
     var KeysizeTLD = mongoose.model('KeysizeTLD', keysizeTLDSchema);
     module.exports = KeysizeTLD;
