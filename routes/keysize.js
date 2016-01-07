969c33d6213e085b8bb4f8a9962858d40cf10037
Georg Kasper <georg.kasper08@gmail.com>
2016-01-07 02:54:19 -0800

    refactoring, removed tabs to whitespaces

--- routes/keysize.js
+++ routes/keysize.js
@@ -8,28 +8,27 @@
 //    var TLD_UNSPECIFIED = "__all";
     
     router.route('/tld').get(function(req, res, next) {
-	// TODO: unspecific does not work, can't find out yet why
-//	var query = {
-//                tld: req.query.tld || TLD_UNSPECIFIED
-//            };
-	
-	keysizeTLD.find(/*query*/).exec(function(err, result) {
-	    if (err) { res.status(500).send(err); }
-	    res.status(200).json(result);
-	    res.end();
-	});
+        // TODO: unspecific does not work, can't find out yet why
+        // var query = {
+        //     tld: req.query.tld || TLD_UNSPECIFIED
+        // };
+        
+        keysizeTLD.find(/*query*/).exec(function(err, result) {
+            if (err) { res.status(500).send(err); }
+            res.status(200).json(result);
+            res.end();
+        });
     });
     
     router.route('/overview').get(function(req, res, next) {
-	KeysizeSummary.find().exec(function(err, result) {
-	    if (err) {
-		res.status(500).send(err);
-	    }
-	    res.status(200).json(result);
-	    res.end();
-	});
+        KeysizeSummary.find().exec(function(err, result) {
+            if (err) {
+                res.status(500).send(err);
+            }
+            res.status(200).json(result);
+            res.end();
+        });
     });
-    
     
     module.exports = router;
 }());
